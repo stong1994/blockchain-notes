5e674c3722f351c5d8fc701ca0ed56453fcba194
suntong <suntong@midu.com>
2019-03-30 13:40:00 +0800

    宠物商店项目完成

--- pet-shop-tutorial/README.md
+++ pet-shop-tutorial/README.md
@@ -5,9 +5,14 @@
 > 在ubuntu中下载的执行文件为`AppImage`格式,需要`chmod a+x *.AppImage`来添加权限,打开后需要添加`workspace`,不然metamask无法连接.
 - [truffle文档](https://truffleframework.com/docs/truffle/getting-started/creating-a-project)
 - [truffle boxes](https://truffleframework.com/docs/truffle/getting-started/creating-a-project),也就是一些truffle的开源项目
+- 如果需要重新migrate合约, 需要添加reset命令,即`truffle migrate --reset`,否则会报`returned values aren't valid did it run out of gas`
+
 
 * 2019.3.28
 解决页面空白问题（`<div id="petTemplate" style="display: none;">` => `<div id="petTemplate" style="display: block;">`）
+
+* 2019.3.30
+按照代码逻辑,页面应该显示16个宠物,28号解决问题中的代码是前端的tmplate模板,dsiplay应该为none,而页面空白的问题是js代码有问题,有个地方少了个逗号.现在已能够正常运行.
 
 ### 遗留问题
 - 2019.3.27 宠物商店的页面显示异常.其他正常.[已解决]
--- pet-shop-tutorial/src/js/app.js
+++ pet-shop-tutorial/src/js/app.js
@@ -34,9 +34,21 @@ App = {
   web3 = new Web3(App.web3Provider);
 
   return App.initContract();
-}
+},
 
   initContract: function() {
+    // 加载Adoption.json，保存了Adoption的ABI（接口说明）信息及部署后的网络(地址)信息，它在编译合约的时候生成ABI，在部署的时候追加网络信息
+$.getJSON('Adoption.json', function(data) {
+  // 用Adoption.json数据创建一个可交互的TruffleContract合约实例。
+  var AdoptionArtifact = data;
+  App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+
+  // Set the provider for our contract
+  App.contracts.Adoption.setProvider(App.web3Provider);
+
+  // Use our contract to retrieve and mark the adopted pets
+  return App.markAdopted();
+});
     /*
      * Replace me...
      */
@@ -49,9 +61,22 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      // 调用合约的getAdopters(), 用call读取信息不用消耗gas
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        if (adopters[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
@@ -59,9 +84,27 @@ App = {
 
     var petId = parseInt($(event.target).data('id'));
 
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    // 获取用户账号
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+
+        // 发送交易领养宠物
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
